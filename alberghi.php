800ceae3b0cc8c34fc913c00fec9ae93f54227c9
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-12-18 18:03:17 +0100

    bootstrap ok

--- alberghi.php
+++ alberghi.php
@@ -45,19 +45,35 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <title>Document</title>
     </head>
     <body>
-    <ul>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">name</th>
+      <th scope="col">description</th>
+      <th scope="col">parking</th>
+      <th scope="col">vote</th>
+      <th scope="col">distanceToCenter</th>
+    </tr>
+  </thead>
+  <tbody>
     <?php
     foreach ($hotels as $hotel) {
-        echo "<li>".$hotel['name']."</li>";
-        echo "<li>".$hotel['description']."</li>";
-        echo "<li>".$hotel['parking']."</li>";
-        echo "<li>".$hotel['vote']."</li>";
-        echo "<li>".$hotel['distance_to_center']."</li>";
+        echo "<tr>";
+        echo "<td>".$hotel['name']."</td>";
+        echo "<td>".$hotel['description']."</td>";
+        echo "<td>".$hotel['parking']."</td>";
+        echo "<td>".$hotel['vote']."</td>";
+        echo "<td>".$hotel['distance_to_center']."</td>";
+        echo "</tr>";
     }
     ?>
-    </ul>     
+  </tbody>
+</table>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>    
     </body>
     </html>
